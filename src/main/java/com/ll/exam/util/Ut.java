56b1af11b7d15eca79ac2c8e14fe77796e92a6c4
jngs-park <dear.jayp@gmail.com>
2022-09-17 21:45:12 +0900

    제너릭을 이용해서 toMap의 기능을 toObj 에 통합

--- src/main/java/com/ll/exam/util/Ut.java
+++ src/main/java/com/ll/exam/util/Ut.java
@@ -3,8 +3,7 @@ package com.ll.exam.util;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.List;
-import java.util.Map;
+
 
 public class Ut {
     public static class json {
@@ -31,15 +30,7 @@ public class Ut {
                 }
         }
 
-        public static <T> List<T> toObj(String jsonStr, TypeReference<List<T>> typeReference, List<T> defaultValue) {
-            try {
-                return om.readValue(jsonStr, typeReference);
-                } catch (JsonProcessingException e) {
-                    return defaultValue;
-                }
-            }
-
-        public static <K, V> Map<K, V> toMap(String jsonStr, TypeReference<Map<K, V>> typeReference, Map<K, V> defaultValue) {
+        public static <T> T toObj(String jsonStr, TypeReference<T> typeReference, T defaultValue) {
             try {
                 return om.readValue(jsonStr, typeReference);
             } catch (JsonProcessingException e) {
