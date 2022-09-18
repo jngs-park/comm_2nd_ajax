fde5fbe17178510a19dab0435447590f9287cfd6
jngs-park <dear.jayp@gmail.com>
2022-09-19 01:04:26 +0900

    채팅방 생성 화면

--- src/main/java/com/ll/exam/DispatchServlet.java
+++ src/main/java/com/ll/exam/DispatchServlet.java
@@ -1,12 +1,12 @@
 package com.ll.exam;
 
 import com.ll.exam.article.ArticleController;
+import com.ll.exam.chat.ChatController;
 import com.ll.exam.member.MemberController;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 @WebServlet("/usr/*")
 public class DispatchServlet extends HttpServlet {
     @Override
@@ -15,10 +15,14 @@ public class DispatchServlet extends HttpServlet {
 
         MemberController memberController = new MemberController();
         ArticleController articleController = new ArticleController();
+        ChatController chatController = new ChatController();
 
         switch (rq.getRouteMethod()) {
             case "GET":
                 switch (rq.getActionPath()) {
+                    case "/usr/chat/createRoom":
+                        chatController.createRoom(rq);
+                        break;
                     case "/usr/article/modify":
                         articleController.showModify(rq);
                         break;
@@ -60,7 +64,6 @@ public class DispatchServlet extends HttpServlet {
                 }
         }
     }
-
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
         doGet(req, resp);
